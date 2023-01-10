ed732ed54f76a4cbf70f556a4d2a4eec84f0bf3f
Gonzalo Brito Gadeschi <gonzalob@nvidia.com>
2023-01-09 17:11:32 -0800

    Improve recipe and build script; add mdspan to recipe

--- ci/recipe.py
+++ ci/recipe.py
@@ -2,6 +2,7 @@
 HPCCM development container for the C++ HPC tutorial
 https://github.com/NVIDIA/hpc-container-maker/
 """
+import platform
 nvhpc_ver = '22.11'
 cuda_ver = '11.8'
 gcc_ver = '12'
@@ -10,13 +11,17 @@ llvm_ver = '16'
 Stage0 += baseimage(image = f'nvcr.io/nvidia/nvhpc:{nvhpc_ver}-devel-cuda{cuda_ver}-ubuntu22.04')
 #Stage0 += baseimage(image = f'nvcr.io/nvidia/nvhpc:{nvhpc_ver}-devel-cuda_multi-ubuntu22.04')
 
+arch = 'x86_64'
+if platform.machine() == 'aarch64':
+    arch = 'aarch64'
+
 Stage0 += packages(ospackages=[
     'libtbb-dev',  # Required for GCC C++ parallel STL
-    'python3', 'python3-pip', 'python-is-python3', 'python3-setuptools',
+    'python3', 'python3-pip', 'python-is-python3', 'python3-setuptools', 'python3-dev',
     'nginx', 'zip', 'make', 'build-essential', 'curl',
     'git', 'bc', 'debianutils', 'libnuma1', 'openssh-client', 'wget', 'numactl',
     # editors
-    'emacs', 'vim',
+#    'emacs', 'vim',
 ])
 
 # Install GNU and LLVM toolchains and CMake
@@ -28,29 +33,39 @@ Stage0 += shell(commands=[
     'set -ex',  # Exit on first error and debug output
 
     # Configure the HPC SDK toolchain to pick the latest GCC
-    f'cd /opt/nvidia/hpc_sdk/Linux_x86_64/{nvhpc_ver}/compilers/bin/',
+    f'cd /opt/nvidia/hpc_sdk/Linux_{arch}/{nvhpc_ver}/compilers/bin/',
     'makelocalrc -d . -x .',
 
     # Install required python packages for the notebooks
     'pip install --upgrade pip',
-    'pip install numpy matplotlib conan gdown jupyterlab ipywidgets',
+    'pip install numpy matplotlib gdown jupyterlab ipywidgets pandas seaborn',
 
-    # Install latest versions of range-v3 and NVIDIA std::execution
+    # Install latest versions of range-v3,  NVIDIA std::execution, and NVTX3
     'mkdir -p /var/tmp',
     'cd /var/tmp',
-    'git clone https://github.com/ericniebler/range-v3.git',
+    'git clone --depth=1 https://github.com/ericniebler/range-v3.git',
     'cp -r range-v3/include/* /usr/include/',
     'rm -rf range-v3',
-    'git clone https://github.com/nvidia/stdexec.git',
+    'git clone --depth=1 https://github.com/nvidia/stdexec.git',
     'cp -r stdexec/include/* /usr/include/',
     'rm -rf stdexec',
+    'git clone --depth=1 --branch=release-v3 https://github.com/NVIDIA/NVTX.git',
+    'cp -r NVTX/c/include/nvtx3 /usr/include/nvtx3',
+    'rm -rf NVTX',
     'cd -',
+
+    # libc++abi: make sure clang with -stdlib=libc++ can find it
+    f'ln -sf /usr/lib/llvm-{llvm_ver}/lib/libc++abi.so.1 /usr/lib/llvm-{llvm_ver}/lib/libc++abi.so',
+
+    # Install HPC SDK mdspan systemwide:
+    f'ln -sf /opt/nvidia/hpc_sdk/Linux_{arch}/{nvhpc_ver}/compilers/include/experimental/mdspan /usr/include/mdspan',
+    f'ln -sf /opt/nvidia/hpc_sdk/Linux_{arch}/{nvhpc_ver}/compilers/include/experimental/__p0009_bits /usr/include/__p0009_bits',
+
+    # Put the labs include directory in the systemwide path:
+    f'ln -sf /src/include /usr/include/labs',
 ])
 
-# libc++abi : make sure clang with -stdlib=libc++ can find it
-Stage0 += shell(commands=[
-    f'ln -sf /usr/lib/llvm-{llvm_ver}/lib/libc++abi.so.1 /usr/lib/llvm-{llvm_ver}/lib/libc++abi.so',
-])
+
 Stage0 += environment(variables={
     'LD_LIBRARY_PATH': f'/usr/lib/llvm-{llvm_ver}/lib:$LD_LIBRARY_PATH',
     'LIBRARY_PATH':    f'/usr/lib/llvm-{llvm_ver}/lib:$LIBRARY_PATH',
@@ -68,7 +83,7 @@ Stage0 += environment(variables={
     # Workaround hwloc binding:
     'OMPI_MCA_hwloc_base_binding_policy': 'none',
 })
-Stage0 += copy(src='labs/', dest='/labs/')
+#Stage0 += copy(src='labs/', dest='/labs/')
 Stage0 += copy(src='include/cartesian_product.hpp', dest='/usr/include/cartesian_product.hpp')
 
 # Install Intel's OneAPI toolchain
